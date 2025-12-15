45376a667a233d7fd2dfc39c3c3f75be940d3383
Sliva4 <slivaktoto@gmail.com>
2025-12-15 20:40:20 +0700

    add info command

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,11 +1,17 @@
 use std::env;
 mod utils;
+const MONITOR_VERSION_CODE: i32 = 2;
+const MODULE_VERSION: &str = "0.02";
 enum Argument {
     Status
 }
+fn info() -> String {
+    let value: String = "Monitor: ".to_owned()+&MONITOR_VERSION_CODE.to_string();
+    return value;
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut arg_type: Argument;;
+    let mut arg_type: Argument;
     for arg in &args[1..] {
         match arg_type {
             Argument::Status => utils::update_status(arg)
@@ -13,6 +19,8 @@ fn main() {
         if arg == "status" {
             arg_type = Argument::Status;
             continue;
+        } else if arg == "info" {
+            println!("{}", info());
         }
     }
 }
