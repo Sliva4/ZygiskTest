807b5f50978013c14f5ac005718ba0ae9a5b3a3b
Sliva4 <slivaktoto@gmail.com>
2025-12-29 15:56:40 +0700

    zh: 0.07

--- zh/src/vars.rs
+++ zh/src/vars.rs
@@ -1,4 +1,4 @@
-pub const ZH_VERSION: &str = "0.06";
+pub const ZH_VERSION: &str = "0.07";
 pub const MODULE_VERSION: &str = "0.03";
 pub const ZH_CONFIG_PATH: &str = "config.toml";
 pub const ZH_SHA256_PATH: &str = "sha256ok";
