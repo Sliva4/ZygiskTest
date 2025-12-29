19756bc4e6ab44bcb90b90f5c1ccb435e01e09e1
Sliva4 <slivaktoto@gmail.com>
2025-12-29 15:17:29 +0700

    Fix sha256 check

--- module/template/customize.sh
+++ module/template/customize.sh
@@ -1,3 +1,8 @@
 sha256sum -c sha256
+if [ $? -eq 0 ]; then
+    ui_print "All checksums matched successfully."
+else
+    ui_print "Some checksums failed or an error occurred."
+fi
 rm -f sha256
 ui_print "Welcome to ZygiskHide!"
