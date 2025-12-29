df7b40b5c03929195b76f983e3666ccc42ee9d93
Sliva4 <slivaktoto@gmail.com>
2025-12-29 15:32:00 +0700

    Try to fix checksums

--- module/template/customize.sh
+++ module/template/customize.sh
@@ -1,9 +1,10 @@
 echo "Path: $MODPATH"
-sha256sum -c $MODPATH/sha256
+cd $MODPATH
+sha256sum -c sha256
 if [ $? -eq 0 ]; then
     ui_print "All checksums matched successfully."
 else
     ui_print "Some checksums failed or an error occurred."
 fi
 ui_print "Welcome to ZygiskHide!"
-rm -f $MODPATH/sha256
+rm -f sha256
