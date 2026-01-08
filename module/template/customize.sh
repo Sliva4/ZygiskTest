97efb9ab46589706abea63df782dd1ab8f36f376
Sliva4 <slivaktoto@gmail.com>
2026-01-08 22:52:42 +0700

    Make customize.sh better

--- module/template/customize.sh
+++ module/template/customize.sh
@@ -1,16 +1,17 @@
 if [[ ${ARCH} != "arm64" ]]; then
-	abort 'Only arm64 is supported!'
+	ui_print '- Only arm64 is supported!'
+    exit 1
 fi
 
-echo "Path: $MODPATH"
+echo "- Path: $MODPATH"
 cd $MODPATH
 sha256sum --status -c sha256
 rm -f sha256
 chmod +x zh
 if [ $? -eq 0 ]; then
-    ui_print "All checksums matched successfully."
+    ui_print "- All checksums matched successfully."
     touch sha256ok
 else
-    ui_print "Some checksums failed or an error occurred."
+    ui_print "- Some checksums failed or an error occurred."
 fi
-ui_print "Welcome to ZygiskHide!"
+ui_print "- Welcome to ZygiskHide!"
